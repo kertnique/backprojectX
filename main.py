128a0352eed5c7297a75334a69c3992b8f3d3c2d
Mark Zegelman <markzegelman@Marks-MacBook-Air.local>
2021-04-09 13:09:42 +0300

    get request model

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
-
-@app.get("/getuser/{username}/")
-async def helloo(username: str):
-    return {"Hello " + username}
+#@app.get("/getuser/{username}/")
+#async def helloo(username: str):
+#    return {"Hello " + username}
   
+
+class Contact(BaseModel):
+    id:int
+    name:str
+    desceiption:str
+@app.get('/contact')
+async def create_contact(contact: Contact):
+    id = contact.id
+    name = contact.name
+    desceiption = contact.desceiption
+
